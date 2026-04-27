bff612f26345c88eb97be45d26d5f1a99eebf755
rienciel03-sketch <rienciel03@gmail.com>
2026-04-28 07:53:58 +0900

    itemsテーブルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,7 @@
+class Item < ApplicationRecord
+ belongs_to :user
+ #has_one :purchase_record
+ #belongs_to :category
+ #belongs_to :condition  
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_18_134948) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_27_223928) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_cost__id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shipping_time_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_18_134948) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
