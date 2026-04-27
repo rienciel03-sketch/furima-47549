d90537bc9b9218e990d31eecc25280c4bf86ee23
rienciel03-sketch <rienciel03@gmail.com>
2026-04-28 08:43:50 +0900

    ActiveHash：カテゴリー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,27 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
+ 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+     redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
 
 
   private
 
-  def items_params
+  def item_params
     params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :shipping_cost__id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
  belongs_to :user
  has_one_attached :image
 
  #has_one :purchase_record
- #belongs_to :category
+ belongs_to :category
  #belongs_to :condition  
 
 
  validates: name, presence: true
  validates: introduction, presence: true
- validates: category_id, presence: true
+ validates: category_id, numericality: { other_than: 1 , message: "can't be blank" }
  validates: condition_id, presence: true
  validates: shipping_cost__id, presence: true
  validates: prefecture_id, presence: true
