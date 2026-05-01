e63f64b11154d1c614df1a500e3381f01471f22c
rienciel03-sketch <rienciel03@gmail.com>
2026-05-01 22:03:16 +0900

    手数料、利益の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,20 @@
-window.addEventListener('load', function () {
+
+
+
+const price = () => {
 
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
-
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    const profitValue = document.getElementById("profit");
+    const taxValue = inputValue * 0.1
+    profitValue.innerHTML = (Math.floor(inputValue - taxValue));
   })
-})
+};
+
+
+window.addEventListener('turbo:load', price);
+window.addEventListener('turbo:render', price);
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create]
+
+  # Chrome DevToolsからの特定のリクエストによるRoutingErrorを抑制する
+  get '/.well-known/appspecific/com.chrome.devtools.json', to: proc { [204, {}, ['']] }
 end
