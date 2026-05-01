50afe2d35f67489155a0a3b6cf4434b79c921a5e
rienciel03-sketch <rienciel03@gmail.com>
2026-05-01 14:14:08 +0900

    ActiveHash

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,5 +23,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :shipping_cost__id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :introduction, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_time_id, :price).merge(user_id: current_user.id)
+ end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,18 @@ class Item < ApplicationRecord
 
  #has_one :purchase_record
  belongs_to :category
- #belongs_to :condition  
+ belongs_to :condition  
+ belongs_to :shipping_cost
+ belongs_to :prefecture
+ belongs_to :shipping_time
 
 
- validates: name, presence: true
- validates: introduction, presence: true
- validates: category_id, numericality: { other_than: 1 , message: "can't be blank" }
- validates: condition_id, presence: true
- validates: shipping_cost__id, presence: true
- validates: prefecture_id, presence: true
- validates: shipping_time_id, presence: true
- validates: price, presence: true
+ validates :name, presence: true
+ validates :introduction, presence: true
+ validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+ validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+ validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank" }
+ validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+ validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank" }
+ validates :price, presence: true
 end
