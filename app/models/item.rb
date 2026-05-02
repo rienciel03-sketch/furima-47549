08ebe61bd60b43ebf0a7f75d63a35fe29920982f
rienciel03-sketch <rienciel03@gmail.com>
2026-05-02 21:44:14 +0900

    修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,6 @@ class Item < ApplicationRecord
  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank" }
  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
  validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank" }
- validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/}
- 
+ validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
 end
