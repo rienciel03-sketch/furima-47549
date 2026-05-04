3785186d237979f58fc0789cadc3f2641afbeee3
rienciel03-sketch <rienciel03@gmail.com>
2026-05-04 22:04:11 +0900

    商品詳細画面へ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
 
   # Chrome DevToolsからの特定のリクエストによるRoutingErrorを抑制する
   get '/.well-known/appspecific/com.chrome.devtools.json', to: proc { [204, {}, ['']] }
