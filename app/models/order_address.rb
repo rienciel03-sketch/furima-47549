6b8f35da8254fecae22bab1605dfeadbc4f74d8e
rienciel03-sketch <rienciel03@gmail.com>
2026-05-22 19:39:56 +0900

    修正２

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -19,5 +19,5 @@ class OrderAddress
      order = Order.create(user_id: user_id, item_id: item_id)
      Address.create(postcode: postcode, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
     end
-   end
+  end
 end
