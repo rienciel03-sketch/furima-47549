13cb3f774292cfdef237c2ddd49aa41955349e6e
rienciel03-sketch <rienciel03@gmail.com>
2026-05-24 23:10:10 +0900

    修正３

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -17,16 +17,18 @@ const pay = () => {
 
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
+        submitButton.removeAttribute("disabled");
       } else {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
+
+        numberElement.clear();
+        expiryElement.clear();
+        cvcElement.clear();
+        document.getElementById("charge-form").submit();
       }
-      numberElement.clear();
-      expiryElement.clear();
-      cvcElement.clear();
-      document.getElementById("charge-form").submit();
     });
     e.preventDefault();
   });
